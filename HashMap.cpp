4448363872ef64493b8dbf4b119543dd4a67b7ea
Kartikkala <kartikkala10december@gmail.com>
2025-04-30 11:42:00 +0530

    Added HashMap

--- HashMap.cpp
+++ HashMap.cpp
@@ -1,74 +1,87 @@
 #include <iostream>
-#include <vector>
-#include <type_traits>
-#include "tree.cpp"
-
 
 template <typename Key, typename Value>
-struct HashNode{
+struct HashNode {
     Key key;
     Value value;
+    HashNode* next;
+
+    HashNode(Key k, Value v) : key(k), value(v), next(nullptr) {}
 };
 
 template <typename Key, typename Value>
 class HashMap {
 private:
-    std::vector<HashNode<Key, BinarySearchTree<Value>*>*> hashTable;
-    const float loadFactorThreshold = 0.7;
-    
-    long hash(Key key) {
+    static const int TABLE_SIZE = 100;
+    HashNode<Key, Value>* table[TABLE_SIZE];
+
+    int hash(Key key) {
         if constexpr (std::is_same<Key, int>::value) {
-            return key % this->hashTable.size();
+            return key % TABLE_SIZE;
         } else if constexpr (std::is_same<Key, char>::value) {
-            return (key - 'A') % this->hashTable.size();
+            return (key - 'A') % TABLE_SIZE;
         } else {
-            throw std::runtime_error("Invalid type");
+            static_assert(sizeof(Key) == 0, "Unsupported key type");
         }
     }
 
 public:
     HashMap() {
-        
-        this->hashTable.resize(100, nullptr);
-    }
-    
-    ~HashMap() {
-        
-        for (auto node : hashTable) {
-            if (node != nullptr) {
-                delete node->value; 
-                delete node;
-            }
-        }
+        for (int i = 0; i < TABLE_SIZE; ++i)
+            table[i] = nullptr;
     }
 
     void push(Key key, Value value) {
         int index = hash(key);
-        if (hashTable[index] == nullptr) {
-            hashTable[index] = new HashNode<Key, BinarySearchTree<Value>*>();
-            hashTable[index]->key = key;
-            hashTable[index]->value = new BinarySearchTree<Value>();
+        HashNode<Key, Value>* head = table[index];
+
+        while (head) {
+            if (head->key == key && head->value == value) {
+                return; // already exists, do nothing
+            }
+            head = head->next;
         }
-        
-        hashTable[index]->value->insert(value);
+
+        HashNode<Key, Value>* newNode = new HashNode<Key, Value>(key, value);
+        newNode->next = table[index];
+        table[index] = newNode;
     }
 
     bool has(Key key, Value value) {
         int index = hash(key);
-        if (hashTable[index] == nullptr) {
-            return false;
+        HashNode<Key, Value>* head = table[index];
+
+        while (head) {
+            if (head->key == key && head->value == value)
+                return true;
+            head = head->next;
         }
-        return hashTable[index]->value->has(value);
+        return false;
+    }
+
+    ~HashMap() {
+        for (int i = 0; i < TABLE_SIZE; ++i) {
+            HashNode<Key, Value>* head = table[i];
+            while (head) {
+                HashNode<Key, Value>* temp = head;
+                head = head->next;
+                delete temp;
+            }
+        }
     }
 };
 
-int main(void)
-{
-    HashMap<char, int> h;
-    h.push('A', 2);
-    h.push('A', 3);
-    h.push('A', 5);
-    h.push('A', 6);
-    h.push('A', 8);
-    std::cout << h.has('A', 0);
+int main() {
+    HashMap<char, int> map;
+
+    map.push('A', 10);
+    map.push('A', 20);
+    map.push('B', 30);
+
+    std::cout << map.has('A', 10) << std::endl; // 1 (true)
+    std::cout << map.has('A', 15) << std::endl; // 0 (false)
+    std::cout << map.has('B', 30) << std::endl; // 1
+    std::cout << map.has('C', 40) << std::endl; // 0
+
+    return 0;
 }
